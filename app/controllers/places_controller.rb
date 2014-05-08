d44fc1cb0c3670fcded845388bf1dedb63e3242b
Chris Williams <vagrant@precise32.(none)>
2014-05-08 01:50:44 +0000

    adding a bunch of user admin stuff

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 
  def index
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-  	redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+  	 redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
   
   def show
@@ -21,12 +25,24 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+        return render :text => 'Not Allowed', :status => :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, :status => :unprocessable_entity
+    end
   end
 
   def update
@@ -37,6 +53,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render :text => 'Now Allowed', :status => forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
